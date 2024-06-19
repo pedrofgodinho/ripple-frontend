c1c5d1cbc2d9458b35950c70a288d12c9b613bbc
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2024-06-19 01:17:44 +0100

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,9 @@
     import "../app.css";
 </script>
 
+<div class="navbar bg-neutral text-neutral-content">
+    <a class="btn btn-ghost text-xl" href="/">Home</a>
+    <a class="btn btn-ghost text-xl" href="/characters">Characters</a>
+    <a class="btn btn-ghost text-xl" href="/echoes">Echoes</a>
+</div>
 <slot />
