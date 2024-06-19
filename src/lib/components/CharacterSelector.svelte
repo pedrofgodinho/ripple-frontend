67585445b9b1cc9adeabbb86ea87498d4373fb48
Pedro Godinho <pedro.fernandez.godinho@gmail.com>
2024-06-19 02:06:23 +0100

    Add character selector

---
+++ src/lib/components/CharacterSelector.svelte
@@ -0,0 +1,18 @@
+<script lang=ts>
+	import type { CharacterMetadata } from "$lib/backend/character";
+	import { Character } from "ripple-calculator";
+
+    export let character: CharacterMetadata;
+</script>
+
+
+<div class="card shadow-xl w-max bg-neutral m-4">
+    <div class="card-body">
+        <h2 class="card-title">{character.name}</h2>
+        <h2>Element: {character.element}</h2>
+        <h2>Weapon Type: {character.weapon}</h2>
+        <div class="card-actions justify-center">
+            <a class="btn btn-primary w-full" href="/characters/{character.id}">Home</a>
+        </div>
+    </div>    
+</div>
--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -2,29 +2,22 @@
     import { onMount } from "svelte";
     import { getCharacters, getCharacter } from "$lib/backend/backend";
 	import type { CharacterMetadata } from "$lib/backend/character";
+	import CharacterSelector from "$lib/components/CharacterSelector.svelte";
 
     let waiting: boolean = true;
     let characters: CharacterMetadata[] = [];
     onMount(async () => {
         characters = await getCharacters();
+        console.log(characters);
         waiting = false;
     });
 
 </script>
 
-<main class="container-fluid">
+<main class="flex flex-wrap">
     {#if !waiting}
-        <ul>
-            {#each characters as character}
-            <article>
-                <h1>{character.name}</h1>
-                <p>Weapon: {character.weapon}</p>
-                <p>Element: {character.element}</p>
-                <a href={`characters/${character.id}`}>
-                    <button>Select</button>
-                </a>
-            </article>
-            {/each}
-        </ul>
+        {#each characters as character}
+            <CharacterSelector character={character}/>
+        {/each}
     {/if}
 </main>
